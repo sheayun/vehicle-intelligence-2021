730d96b836f37c3b0a7d72e7c7d887ba6e1c3f94
SeungUkJeon <80089347+SeungUkJeon@users.noreply.github.com>
2021-05-07 21:46:13 +0900

    Add classifier to HW_week6

--- code/week-6/GNB/classifier.py
+++ code/week-6/GNB/classifier.py
@@ -28,23 +28,30 @@ class GNB():
     # Train the GNB using a combination of X and Y, where
     # X denotes the observations (here we have four variables for each) and
     # Y denotes the corresponding labels ("left", "keep", "right").
+    
     def train(self, X, Y):
-        '''
-        Collect the data and calculate mean and standard variation
-        for each class. Record them for later use in prediction.
-        '''
-        # TODO: implement code.
-
-    # Given an observation (s, s_dot, d, d_dot), predict which behaviour
-    # the vehicle is going to take using GNB.
+            self.X_arr, self.Y_arr = np.array(X), np.array(Y)
+            unique, cnt = np.unique(Y, return_counts=True)
+            self.priors = cnt / len(Y)
+        
+            self.Means = np.array([self.X_arr[np.where(self.Y_arr==i)].mean(axis=0) for i in self.classes])
+            self.Standevi = np.array([self.X_arr[np.where(self.Y_arr==i)].std(axis=0) for i in self.classes])
+        
+            return self
+        
     def predict(self, observation):
-        '''
-        Calculate Gaussian probability for each variable based on the
-        mean and standard deviation calculated in the training process.
-        Multiply all the probabilities for variables, and then
-        normalize them to get conditional probabilities.
-        Return the label for the highest conditional probability.
-        '''
-        # TODO: implement code.
-        return "keep"
+            probas = np.zeros(len(self.classes))
+            # Calculate Gaussian probability for each variable based on the
+            # mean and standard deviation calculated in the training process.
+            for i in range(len(self.classes)):
+                probability = 1
+                for k in range(self.X_arr.shape[1]):
+                    # Multiply all the probabilities for variables, and then
+                    # normalize them to get conditional probabilities.
+                    probability *= gaussian_prob(observation[k], self.Means[i][k], self.Standevi[i][k])
 
+                probas[i] = probability
+            nmz = probas / probas.sum()
+        
+            # Return the label for the highest conditional probability.
+            return self.classes[nmz.argmax(axis=0)]
